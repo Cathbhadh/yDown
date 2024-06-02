099e8a62f475d39ce5c7235162a6c7115242a880
Cathbhadh <36742410+Cathbhadh@users.noreply.github.com>
2024-06-02 16:04:42 +0300

    Update down.py

--- down.py
+++ down.py
@@ -2,9 +2,9 @@ import streamlit as st
 import requests
 from datetime import datetime
 import zipfile
-import os
 from io import BytesIO
 import time
+from urllib.parse import urlparse, parse_qs, urlunparse
 
 BASE_URL = "https://api.yodayo.com/v1/users/{user_id}/posts"
 LIMIT = 500
@@ -30,17 +30,14 @@ def filter_posts_by_date(posts, start_date, end_date):
 @st.cache_data(ttl=3200)
 def clean_url(url):
     original_url = url
-    # Remove parameters from the URL
-    url_parts = url.split("_")
-    if len(url_parts) > 1:
-        base_url = url_parts[0]
-        extension = url.split(".")[-1]
-        if extension in ["jpg", "png"]:
-            cleaned_url = base_url + "." + extension
-        else:
-            cleaned_url = base_url
-    else:
-        cleaned_url = url
+    parsed_url = urlparse(url)
+    query_params = parse_qs(parsed_url.query)
+
+    # Remove width and height parameters
+    query_params.pop('width', None)
+    query_params.pop('height', None)
+
+    cleaned_url = urlunparse(parsed_url._replace(query='&'.join([f"{key}={value[0]}" for key, value in query_params.items()])))
 
     try:
         response = requests.head(cleaned_url, timeout=0.2)
@@ -69,12 +66,8 @@ def main():
     st.title("Yodayo Image Downloader")
 
     user_id = st.text_input("Enter User ID", "")
-    start_date = st.text_input(
-        "Enter Start Date (YYYY-MM-DDTHH:MM:SSZ)", "2024-05-27T00:00:00Z"
-    )
-    end_date = st.text_input(
-        "Enter End Date (YYYY-MM-DDTHH:MM:SSZ)", "2024-05-28T00:00:00Z"
-    )
+    start_date = st.text_input("Enter Start Date (YYYY-MM-DDTHH:MM:SSZ)", "2024-05-27T00:00:00Z")
+    end_date = st.text_input("Enter End Date (YYYY-MM-DDTHH:MM:SSZ)", "2024-05-28T00:00:00Z")
 
     if st.button("Download Images"):
         if user_id and start_date and end_date:
